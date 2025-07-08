ba1d1a4c4dbdbb9975eb3b51ad93f207b3cc786a
elias-dzobo <elias.dzobo@gmail.com>
2025-07-08 12:03:32 +0000

    feat: first working version

--- src/agent/main.py
+++ src/agent/main.py
@@ -1,31 +1,34 @@
 """ A script to generate logical data using Groq's LLM capabilities. """
 
 #internal 
-from prompts.main import SYSTEM_PROMPT
-from schema.main import Response
+from src.schema.main import Response
 import instructor
 
 
 #external 
+from dotenv import load_dotenv
 from groq import Groq
+
+load_dotenv()
 
 client = instructor.from_groq(Groq())
 
 
 def generate_logical_data(messages, query):
-    chat_completion = client.chat.completions.create(
-        messages.append(
+    messages.append(
             {
                 "role": "user",
                 "content": query,
             }
-        ),
+        )
+    chat_completion = client.chat.completions.create(
+        messages=messages,
         # The language model which will generate the completion.
         model="llama-3.3-70b-versatile",
         response_model=Response
     )
     # Print the completion returned by the LLM.
-    response = chat_completion.choices[0].message.content
+    response = chat_completion
 
     messages.append(
         {
